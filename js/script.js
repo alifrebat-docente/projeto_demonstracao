7d93b8c7e08bc76fc57ba3cd1c9276ff885b8c43
Alifrebat <alifrebat@gmail.com>
2026-04-16 14:50:40 -0300

    Criando função de captura do submit

--- js/script.js
+++ js/script.js
@@ -5,3 +5,23 @@ const divLista = document.querySelector('#div-lista')
 //CRIANDO O ARRAY DE PESSOAS
 const pessoas = []
 
+//CAPTURANDO O EVENTO SUBMIT DO FORMULÁRIO
+formulario.addEventListener('submit', (evt) => {
+    evt.preventDefault()
+    const dadosPessoa = new FormData(formulario)
+
+    let sexoSelecionado = dadosPessoa.get(sexo).checked
+    
+    let idade = calcIdade(dadosPessoa.get('data-nascimento'))
+
+    const Pessoa = {
+        nome : dadosPessoa.get('nome'),
+        sexo : sexoSelecionado,
+        idade : idade,
+        peso : dadosPessoa.get('peso'),
+        altura : dadosPessoa.get('altua')
+    }
+
+})
+
+

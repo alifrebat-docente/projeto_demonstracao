d5eb4425627e79696f2d1ad2605efde8010e0044
Alifrebat <alifrebat@gmail.com>
2026-04-16 14:30:05 -0300

    Versionando o arquivo script.js

---
+++ js/script.js
@@ -0,0 +1,7 @@
+//PEGANDO ELEMENTOS DO DOM
+const formulario = document.querySelector('#formulario')
+const divLista = document.querySelector('#div-lista')
+
+//CRIANDO O ARRAY DE PESSOAS
+const pessoas = []
+

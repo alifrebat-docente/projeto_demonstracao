3a80fc456f6e7ce7f5d8b50bb214abc25e4de779
alifrebat <alifrebat@gmail.com>
2026-04-24 08:55:58 -0300

    Capturado o evento do click no botão

--- js/scripts_imc.js
+++ js/scripts_imc.js
@@ -6,3 +6,19 @@ const divLista = document.querySelector("#div-lista")
 //ARRAY PESSOA
 const pessoas = []
 
+//CAPTURANDO O EVENTO DE CLICK NO BOTÃO ENVIAR
+btnEnviar.addEventListener('click', (evt) => {
+    evt.preventDefault()
+
+    const formPessoa = new FormData(formImc)
+
+    const objPessoa = {
+        nome: formPessoa.get('nome'),
+        sexo: formPessoa.get('sexo'),
+        data_nascimento: formPessoa.get('data-nascimento'),
+        peso: formPessoa.get('peso'),
+        altura: formPessoa.get('altura')
+
+    }
+
+})

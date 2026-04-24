3ead0e6fb22c1d2d8f4eaf4910e798ea29eb19ee
alifrebat <alifrebat@gmail.com>
2026-04-24 09:09:23 -0300

    Criado o arquivo script_api.js. criado o método para salvar dados

---
+++ js/script_api.js
@@ -0,0 +1,26 @@
+
+const salvarDados = async (objPessoa) => {
+    const endPoint = 'https://localhost:7015/api/Pessoa'
+
+    try {
+        const resposta = await fetch(
+            endPoint, {
+            method: 'POST',
+            headers: { 'Conten-Type': "application/json" },
+            body: JSON.stringify(objPessoa)
+        })
+
+        if(!resposta.ok ){
+            const txtErro = await resposta.text();
+                throw new Error(txtErro);
+        }
+
+        const dados = await resposta.json()
+        return dados
+        
+    } catch (erro) {
+        console.log("ERRO AO CADASTRAR ", erro)
+    }
+
+}
+
